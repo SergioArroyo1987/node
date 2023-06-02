d0d86e064f8e6263840cc26447e9096e36a043ab
sergio Arroyo <chupi1987@hotmail.com>
2023-06-02 12:13:53 +0200

    sdfsdf

--- tema1/dia1.js
+++ tema1/dia1.js
@@ -1,5 +1,5 @@
 
-const readlineSync=require('readline-sync');
+const readline=require('readline');
 const { log } = require('console');
 const datos=require('fs');
 console.log("mensaje1");
@@ -14,27 +14,46 @@ setTimeout(()=>{
         let persona={
             name:"Sergio",
             surname:"Arroyo",
-            age:35
+            age:37
         }
         let enviar=JSON.stringify(persona);
-        datos.writeFileSync("prueba.json",enviar);
-        let leer=datos.readFileSync("prueba.json","utf-8");
-        leer=JSON.parse(leer);
-        console.log(leer);
+        
+        datos.writeFile("prueba.json",enviar,()=>{
+            datos.readFile("prueba.json","utf-8",(err,data)=>{
+                console.log(data,"hola");
+            });
+        //let leer=JSON.parse(readFile called);
+        
+        });
+        
     },1000);
 
 setTimeout(()=>{
-    let nombre=readlineSync.question("Introduca nombre:");
-    let apellido=readlineSync.question("Introduca nombre:");
-    let age=readlineSync.question("Introduca edad:");
-    persona.name=nombre;
-    persona.surname=apellido;
-    persona.age=age;
-    let enviar=JSON.stringify(persona);
-    datos.writeFileSync("prueba.json",enviar);
-    let leer=datos.readFileSync("prueba.json","utf-8");
-        leer=JSON.parse(leer);
-        console.log(leer);
+    let rl = readline.createInterface(
+        process.stdin, process.stdout);
+    rl.question("Introduca nombre:",(nombre)=>{
+         rl.question("Introduca nombre:",(apellido)=>{
+            rl.question("Introduca edad:",(age)=>{
+                persona.name=nombre;
+                persona.surname=apellido;
+                persona.age=age;
+                let enviar=JSON.stringify(persona);
+                
+                datos.writeFile("prueba.json",enviar,()=>{
+                    datos.readFile("prueba.json","utf-8",(err,data)=>{
+                        //leer=JSON.parse(leer);
+                     console.log(data);
+                     rl.close();
+                    });
+                    
+            });
+        });
+                });
+    
+    });
+    
+    
+    
 
 
 },2000);
